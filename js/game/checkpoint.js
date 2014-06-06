9c3958429471362219674b709a84384a5b367ba8
woofers <woofers1tt@gmail.com>
2014-06-05 20:11:38 -0700

    Remove Git Stuff

--- js/game/checkpoint.js
+++ js/game/checkpoint.js
@@ -31,18 +31,12 @@ checkpoint.prototype = {
     },
 
     activate : function(player, checkpointGroup){
-<<<<<<< HEAD
 
         sav.x = checkpointGroup.x - 300;
 
         if (!checkpointGroup.activate === true)
         {
-=======
-
-        if (!checkpointGroup.activate === true)
-        {
             sav.x = checkpointGroup.x - 300;
->>>>>>> ae7ee62b19b91bfb2f0e9fd6d43a1b51203e22ca
             checkpointGroup.activate = true;
             checkpointGroup.animations.play('activate');
         }
